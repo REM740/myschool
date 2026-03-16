ba3a25b42d02a223333860fe45f9d71f4bded87d
Remar Jude G Alonzo <153622231+REM740@users.noreply.github.com>
2026-03-16 10:57:04 +0800

    Initial Commit

--- college/views.py
+++ college/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def collegeGreet(request):
-    return HttpResponse("Hello College!")
+    return HttpResponse("Hello HighSchool!")
